8709ee574cb04743f4f4c4b10ae69a507994d537
TheWisePigeon <zozozozeph@gmail.com>
2022-11-24 00:42:36 +0000

    ref: updated entity creation logic

--- src/services/Entity.ts
+++ src/services/Entity.ts
@@ -1,17 +1,11 @@
 import { Request, Response } from "express"
 import Entity from "../models/Entity"
 import ContentContainer from "../models/ContentContainer"
-import mongoose from "mongoose"
 
 export async function getEntities(req: Request, res: Response) {
     try {
         const { decoded } = req.body
         const { container } = req.query
-        if( !mongoose.isValidObjectId(container) ){
-            return res.status(400).send({
-                "message":"Invalid container id"
-            })
-        }
         const { email } = decoded
         const entities = await Entity.find({ owner: email, container })
         return res.status(200).send({
@@ -27,13 +21,10 @@ export async function getEntities(req: Request, res: Response) {
 export async function createEntity( req: Request, res: Response ){
     try {
         const { decoded, name, fields, container } = req.body
-        if( !mongoose.isValidObjectId(container) ){
-            return res.status(400).send({ "message":"Invalid container id" })
-        }
-        if( ! await ContentContainer.findById(container) ){
+        const { email } = decoded
+        if( ! await ContentContainer.findOne({ name: container, owner: email }) ){
             return res.status(404).send({ "message":"Container not found" })
         }
-        const { email } = decoded
         const newEntity = new Entity({ name, fields, container, owner: email })
         newEntity.save((err, result)=>{
             if(err){
